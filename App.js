ef474ad98363171c754337b0ab28b5fafd295ec6
余洋洋 <yoyouyou@yuyangyangdeMacBook-Pro.local>
2018-11-01 16:49:37 +0800

    加入picker的使用

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, TextInput, Button, Alert, Picker, TouchableOpacity } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -33,24 +33,59 @@ class Blink extends Component {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <Image source={pic} style={{ width: 193, height: 110, display: this.state.showText }} />
+      <Image source={pic} style={{ width: 193, height: 110 }} />
     )
   }
 }
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      language: ''
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
         <Blink />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
         <Text style={styles.welcome}>Hello world!</Text>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button onPress={() => {
+          Alert.alert('you have clicked the button!')
+        }} title="点我"></Button>
+
+        <Picker selectedValue={this.state.language} mode={Picker.MODE_DIALOG} style={{ width: 200 }} prompt='请选择语言'
+          enabled={true}
+          itemStyle={{ fontSize: 25, color: 'red', textAlign: 'left', fontWeight: 'bold' }} onValueChange={(lang) => this.setState({ language: lang })}>
+          <Picker.Item label='Java' value='java' />
+          <Picker.Item label="JavaScript" value="js" />
+          <Picker.Item label="C++" value="cpp" />
+          <Picker.Item label="Swift" value="swift" />
+        </Picker>
+        <TouchableOpacity onPress={this.onPress.bind(this, this.state.language)}>
+          <Text>确定</Text>
+        </TouchableOpacity>
       </View >
     );
   }
+
+  onPress(language) {
+    alert(language)
+  }
+
 }
 
 const styles = StyleSheet.create({
