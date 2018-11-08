af9498729c29822d042e3c156b22824dc5f95eec
余洋洋 <yoyouyou@yuyangyangdeMacBook-Pro.local>
2018-11-08 16:28:54 +0800

    自定义导航栏的样式

--- src/stacks/detail.js
+++ src/stacks/detail.js
@@ -3,6 +3,15 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 
 export default class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    headerRight: (
+      <Button
+        onPress={() => alert("This is a button!")}
+        title="分享"
+        color="#f00"
+      />
+    )
+  };
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
